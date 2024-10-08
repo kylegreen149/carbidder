c1393927a15317a8bde88d47ca1b0ea6321e40e4
Kyle Greenidge <kylegreen149@gmail.com>
2024-10-08 14:36:53 -0400

    Added comments, test car to database

--- src/components/CarPage.js
+++ src/components/CarPage.js
@@ -11,13 +11,14 @@ function CarPage() {
         const searchWords = searchText.toUpperCase().split(" ") // Split search text into words to include spaces in search
 
         return (
+            // Uses .some() to map over the data to see if anything matches with anything in the search bar
             searchWords.some(word =>
-                car.color.toUpperCase().includes(word) ||
-                car.year.toString().includes(word) ||
-                car.brand.toUpperCase().includes(word) ||
-                car.model.toUpperCase().includes(word) ||
-                car.body_style.toUpperCase().includes(word) ||
-                car.type.toUpperCase().includes(word)
+                car.color.toUpperCase().includes(word) || // Searches car color OR
+                car.year.toString().includes(word) || // Searches car year OR
+                car.brand.toUpperCase().includes(word) || // Searches car brand OR
+                car.model.toUpperCase().includes(word) || // Searches car model OR
+                car.body_style.toUpperCase().includes(word) || // Searches car body style OR
+                car.type.toUpperCase().includes(word) // Searches car type
             )
         )
     })
