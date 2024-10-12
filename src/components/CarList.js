5680f23a90ea9f50dfbfc3743e60bacc8a799994
Kyle Greenidge <kylegreen149@gmail.com>
2024-10-11 23:33:33 -0400

    Correctly installed react bootstrap

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -1,4 +1,6 @@
 import { useOutletContext } from "react-router-dom"
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 // import { useState } from "react";
 
 function CarList({car}) {
@@ -13,26 +15,44 @@ function CarList({car}) {
     }
     
     return (
-        <>
-            <div>
-                <img src={car.image} alt={`${car.year} ${car.brand} ${car.model}`} style={{height: "50%", width: "50%" }}/>
-                <h2>{car.year} {car.brand} {car.model}</h2>
-                <p><b>Color: </b>{car.color}</p>
-                <p><b>Body Style: </b>{car.body_style}</p>
-                <p><b>Type: </b>{car.type}</p>
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src={car.image} alt={`${car.year} ${car.brand} ${car.model}`} style={{height: "50%", width: "50%" }} />
+          <Card.Body>
+            <Card.Title>{car.year} {car.brand} {car.model}</Card.Title>
+            <Card.Text>
                 <p><b>Mileage: </b> {car.mileage} mi.</p>
-                <p><b>Top Speed (mph): </b> {car.top_speed}</p>
-                <p><b>Accident History: </b>{car.accident_history}</p>
-                <p><b>Modifications: </b>{car.modifications}</p>
                 <p><b>Bids: </b>{car.bids}</p>
                 <p><b>Starting Bid Price: </b>${car.starting_bid_price}</p>
                 <p><b>Current Bid Price: </b>${car.current_bid_price}</p>
-                <button onClick={handleClick}>Place Bid</button>
 
-            </div>
-            <br></br>
-        </>
-    )
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+      );
+
+
+    // return (
+    //     <>
+    //         <div>
+    //             <img src={car.image} alt={`${car.year} ${car.brand} ${car.model}`} style={{height: "50%", width: "50%" }}/>
+    //             <h2>{car.year} {car.brand} {car.model}</h2>
+    //             <p><b>Color: </b>{car.color}</p>
+    //             <p><b>Body Style: </b>{car.body_style}</p>
+    //             <p><b>Type: </b>{car.type}</p>
+    //             <p><b>Mileage: </b> {car.mileage} mi.</p>
+    //             <p><b>Top Speed (mph): </b> {car.top_speed}</p>
+    //             <p><b>Accident History: </b>{car.accident_history}</p>
+    //             <p><b>Modifications: </b>{car.modifications}</p>
+    //             <p><b>Bids: </b>{car.bids}</p>
+    //             <p><b>Starting Bid Price: </b>${car.starting_bid_price}</p>
+    //             <p><b>Current Bid Price: </b>${car.current_bid_price}</p>
+    //             <button onClick={handleClick}>Place Bid</button>
+
+    //         </div>
+    //         <br></br>
+    //     </>
+    // )
 }
 
 export default CarList
